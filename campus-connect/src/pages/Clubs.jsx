ce33885e3ed56aaefbd8234678b9bb95d660ac5a
Nanimathala <147790405+Nanimathala@users.noreply.github.com>
2026-06-04 17:19:15 +0530

    dashboard created

--- campus-connect/src/pages/Clubs.jsx
+++
@@ -1,9 +0,0 @@
-function Clubs() {
-  return (
-    <main style={{ padding: "32px" }}>
-      <h1>Clubs</h1>
-    </main>
-  );
-}
-
-export default Clubs;
--- campus-connect/src/pages/Profile.jsx
+++
@@ -1,9 +0,0 @@
-function Profile() {
-  return (
-    <main style={{ padding: "32px" }}>
-      <h1>Profile</h1>
-    </main>
-  );
-}
-
-export default Profile;
