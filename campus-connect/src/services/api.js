c7e5e6fd6b63fee6b903d4321ef4fd7ba41cfc4e
Nanimathala <147790405+Nanimathala@users.noreply.github.com>
2026-06-18 11:23:59 +0530

    ...

--- campus-connect/src/services/api.js
+++ campus-connect/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://didactic-capybara-7v9g6g9ppr7xhxp54-8080.app.github.dev/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
